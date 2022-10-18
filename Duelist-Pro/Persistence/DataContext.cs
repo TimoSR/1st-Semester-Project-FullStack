c4cdfa45e56e5375c24823bacb7c151212db19cc
Archit3ct <timothy.s.rasmussen@gmail.com>
2022-10-18 23:18:25 +0200

    Update Project

--- Duelist-Pro/Persistence/DataContext.cs
+++ Duelist-Pro/Persistence/DataContext.cs
@@ -2,12 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
     public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Activity> MyProperty { get; set; }
+
+
     }
 }
