baf51c4700b6aaa5e6968a7fb247cca32b816930
Archit3ct <timothy.s.rasmussen@gmail.com>
2022-11-08 23:03:18 +0100

    Working with form input

--- Duelist-Pro/client-app/src/app/models/activity.ts
+++ Duelist-Pro/client-app/src/app/models/activity.ts
@@ -1,6 +1,9 @@
 export interface IActivity {
     id: string
     title: string
+    /** Date needs to be of type string
+     *  as HTML date requires that to render inside our form
+     */
     date: string
     description: string
     category: string
--- Duelist-Pro/client-app/src/features/activities/form/ActivityForm.tsx
+++ Duelist-Pro/client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 
@@ -7,14 +7,40 @@ interface Props {
     closeForm: () => void;
 }
 
-export default function ActivityForm({activity, closeForm}: Props) {
+export default function ActivityForm({activity: selectedActivity, closeForm}: Props) {
+
+    /** If activity is null, we initilize an empty activity */
+    const initialState = selectedActivity ?? {
+        id: '',
+        title: '',
+        category: '',
+        description: '',
+        date: ''
+    }
+
+    const [activity, setActivity] = useState(initialState);
+
+    function handleSumit() {
+        console.log(activity);
+    }
+
+    function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        const {name, value} = event.target;
+        /** 
+         * Spreading the existing properties of the activity (copying the object in best manner)
+         * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+         * Property with key of name is set to whatever the value is.
+        */
+        setActivity({...activity, [name]: value})
+    }
+
     return(
         <Segment clearing>
-            <Form>
-                <Form.Input placeholder="Title" />
-                <Form.Input placeholder="Date" />
-                <Form.Input placeholder="Description" />
-                <Form.Input placeholder="Category" />
+            <Form onSubmit={handleSumit} autoComplete='off'>
+                <Form.Input placeholder="Title" value={activity.title} name="title" onChange={handleInputChange} />
+                <Form.Input placeholder="Date" value={activity.date} name="date" onChange={handleInputChange} />
+                <Form.TextArea placeholder="Description" value={activity.description} name="description" onChange={handleInputChange} />
+                <Form.Input placeholder="Category" value={activity.category} name="category" onChange={handleInputChange} />
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel' color='red' />
                 <Button floated='right' color='blue' content='Submit'></Button>            
             </Form>
