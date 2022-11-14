ea9dfd581c7ef706c17e624ce59858a23478c233
Archit3ct <timothy.s.rasmussen@gmail.com>
2022-11-14 14:44:43 +0100

    Making it more readable

--- Duelist-Pro/client-app/src/app/agent.ts
+++ Duelist-Pro/client-app/src/app/agent.ts
@@ -36,16 +36,16 @@ const responseBody = <T> (response: AxiosResponse<T>) => response.data;
 
 const requests = {
     get: <T> (url: string) => axios.get<T>(url).then(responseBody),
-    post: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
-    put: <T> (url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+    create: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    update: <T> (url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
     delete: <T> (url: string) => axios.delete<T>(url).then(responseBody),
 };
 
 const Activities = {
     list: () => requests.get<IActivity[]>('/activities'),
     details: (id: string) => requests.get<IActivity>(`/activities/${id}`),
-    create: (activity: IActivity) => requests.post<void>(`/activities`, activity),
-    update: (activity: IActivity) => requests.put<void>(`/activities/${activity.id}`, activity),
+    create: (activity: IActivity) => requests.create<void>(`/activities`, activity),
+    update: (activity: IActivity) => requests.update<void>(`/activities/${activity.id}`, activity),
     delete: (id: string) => requests.delete<void>(`/activities/${id}`)
 };
 
