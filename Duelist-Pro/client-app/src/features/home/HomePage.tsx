9f001b0c050847231c3ee2361523590e26c0f749
Archit3ct <timothy.s.rasmussen@gmail.com>
2022-11-28 14:17:22 +0100

    Finished Styling

--- Duelist-Pro/client-app/src/features/home/HomePage.tsx
+++ Duelist-Pro/client-app/src/features/home/HomePage.tsx
@@ -6,12 +6,12 @@ export default function HomePage() {
     return (
         <Segment inverted textAlign='center' className='masthead'>
             <Container text>
-                <Header as='h1' inverted>
+                <Header as='h1' inverted content="Welcome to Coup'Create">
                     {/* <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}}/>
                     Coup'Create */}
                 </Header>
-                <Header as='h2' inverted content="Welcome to Coup'Create"></Header>
-                <Button as={Link} to='/activities' size='huge' inverted>
+                {/* <Header as='h2' inverted content="Welcome to Coup'Create"></Header> */}
+                <Button style={{marginTop: 25}} as={Link} to='/activities' size='huge' inverted>
                     Take me to the Activities!
                 </Button>
             </Container>
