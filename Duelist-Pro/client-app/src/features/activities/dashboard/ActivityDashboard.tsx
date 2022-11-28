1973561f0ee80df5ccf6f8ea1343489d29abba9d
Archit3ct <timothy.s.rasmussen@gmail.com>
2022-11-28 13:36:38 +0100

    Finishing the styling

--- Duelist-Pro/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Duelist-Pro/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
 import LoadingComponent from '../../../app/layout/LoadingComponents';
 import { useStore } from '../../../app/stores/store';
+import ActivityFilters from './ActivityFilters';
 import ActivityList from './ActivityList';
 
 /**  
@@ -43,7 +44,7 @@ export default observer(function ActivityDashBoard() {
                 {/* {(editMode) ?
                     <ActivityForm /> : null}  */}
 
-                <h2>Activity Filters</h2>
+                <ActivityFilters />
 
             </Grid.Column>
         </Grid>
