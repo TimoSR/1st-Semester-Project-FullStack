1928d1a1c655e7361b469d0bfeb6346be49eacdc
Archit3ct <timothy.s.rasmussen@gmail.com>
2022-12-04 16:14:59 +0100

    Adding Users

--- Duelist-Pro/API/Program.cs
+++ Duelist-Pro/API/Program.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -29,13 +31,12 @@ namespace API
             try
             {
                 var context = services.GetRequiredService<DataContext>();
-
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 /*
                     MigrateAsync and SeedData is only usefull during development
                 */
-
                 await context.Database.MigrateAsync();
-                await Seed.SeedData(context);
+                await Seed.SeedData(context, userManager);
 
             }
             catch (Exception ex)
