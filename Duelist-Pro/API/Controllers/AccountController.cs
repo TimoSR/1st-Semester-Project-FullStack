8ecd281dd522bf239ac80df4e97bc460070cef65
Archit3ct <timothy.s.rasmussen@gmail.com>
2022-12-05 13:02:29 +0100

    Current User Token can now get received

--- Duelist-Pro/API/Controllers/AccountController.cs
+++ Duelist-Pro/API/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.DTO;
 using API.Services;
@@ -43,6 +44,11 @@ namespace API.Controllers
 
             var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
 
+            if (result.Succeeded)
+            {
+                return CreateUserObject(user);
+            }
+
             return Unauthorized();
         }
 
@@ -72,17 +78,31 @@ namespace API.Controllers
 
             if (result.Succeeded)
             {
-                return new UserDto
-                {
-                    DisplayName = user.DisplayName,
-                    Image = null,
-                    Token = _tokenService.CreateToken(user),
-                    UserName = user.UserName
-                };
+                return CreateUserObject(user);
             }
 
-            return BadRequest("Problem reistering user");
+            return BadRequest("Problem registering user");
 
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+            return CreateUserObject(user);
+        }
+
+        private UserDto CreateUserObject(AppUser user)
+        {
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Image = null,
+                Token = _tokenService.CreateToken(user),
+                UserName = user.UserName
+            };
         }
 
     }
