4d3ee9b396e7da0f777797615efcdefeb64ff1ba
Archit3ct <timothy.s.rasmussen@gmail.com>
2022-12-06 23:28:26 +0100

    Adding the Dto's

--- Duelist-Pro/Application/Activities/List.cs
+++ Duelist-Pro/Application/Activities/List.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Application.Core;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -70,14 +71,23 @@ namespace Application.Activities
                     We send a request with the entity framework to the database
                 */
 
+                /* This is called eargerly loading*/
+                // var activities = await _context.Activities
+                //     .Include(a => a.Attendees)
+                //     .ThenInclude(u => u.AppUser)
+                //     .ToListAsync(cancellationToken);
+
+                /* Projection will help us with a cleaner query when joining tables */
                 var activities = await _context.Activities
-                    .Include(a => a.Attendees)
-                    .ThenInclude(u => u.AppUser)
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
                     .ToListAsync(cancellationToken);
 
-                var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
+                /* When projecting we dont need the mapping */
+                // var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
 
-                return Result<List<ActivityDto>>.Success(activitiesToReturn);
+                // return Result<List<ActivityDto>>.Success(activitiesToReturn);'
+
+                return Result<List<ActivityDto>>.Success(activities);
 
             }
         }
